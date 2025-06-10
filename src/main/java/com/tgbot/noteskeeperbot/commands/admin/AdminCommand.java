c5d7b7174b0126db682be239b7ed5eae48d595c1
puh0v <puh7794@gmail.com>
2025-06-10 15:51:50 +0300

    refactor: заменено логгирование в AdminCommand

--- src/main/java/com/tgbot/noteskeeperbot/commands/admin/AdminCommand.java
+++ src/main/java/com/tgbot/noteskeeperbot/commands/admin/AdminCommand.java
@@ -13,6 +13,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import java.util.List;
 
@@ -24,6 +26,7 @@ public class AdminCommand implements Commands {
     private final UserRegistryService userRegistry;
     private final FlagManager flagManager;
     private final MessageSender messageSender;
+    private static final Logger logger = LoggerFactory.getLogger(AdminCommand.class);
 
     public AdminCommand(UserRegistryService userRegistry, FlagManager flagManager, MessageSender messageSender) {
         this.userRegistry = userRegistry;
@@ -38,31 +41,44 @@ public class AdminCommand implements Commands {
 
     @Override
     public void execute(Long userId, String userMessage, Update update, TelegramBotService telegramBotService) {
+
         if (userMessage.equals(getCommandName()) && userId == adminId) {
+            logger.info("Поступила команда от администратора...");
+
             flagManager.resetFlag(userId);
             SendMessage message = new SendMessage(userId.toString(), "Отправьте текст для рассылки " +
                     "другим пользователям.\n\nДля отмены рассылки отправьте команду \"/cancel\"");
-            messageSender.sendMessageToUser(userId, message, telegramBotService);
 
+            messageSender.sendMessageToUser(userId, message, telegramBotService);
             flagManager.setFlag(userId, getCommandName());
+
         } else if (flagManager.flagHasThisCommand(userId, getCommandName())) {
+            logger.info("Поступил ответ администратора (по флагу)...");
+
             if (userMessage.equals("/cancel")) {
+                logger.info("Администратор отменил рассылку пользователям");
+
                 SendMessage message = new SendMessage(userId.toString(), "Рассылка отменена");
                 messageSender.sendMessageToUser(userId, message, telegramBotService);
 
                 flagManager.resetFlag(userId);
-            }
-            else {
+
+            } else {
                 List<UsersEntity> listOfUsers = userRegistry.getAllUsers();
+
                 if (update.getMessage().hasText() && !update.getMessage().hasPhoto()) {
+                    logger.info("Подготовка к рассылке текстового сообщения...");
+
                     for (UsersEntity user : listOfUsers) {
                         Long id = user.getUserId();
                         SendMessage message = new SendMessage(id.toString(), userMessage);
                         messageSender.sendMessageToUser(id, message, telegramBotService);
                     }
                     flagManager.resetFlag(userId);
-                }
-                else if (update.getMessage().hasPhoto()) {
+
+                } else if (update.getMessage().hasPhoto()) {
+                    logger.info("Подготовка к рассылке изображения...");
+
                     List<PhotoSize> photoList = update.getMessage().getPhoto();
                     PhotoSize largestImage = photoList.get(photoList.size() - 1);
                     String fileId = largestImage.getFileId();
