fa72d6895f9098fd064bcee828d9de132559311f
puh0v <puh7794@gmail.com>
2025-06-11 11:42:37 +0300

    refactor: Заменено логгирование в NotesPageBuilder, улучшена читаемость кода, убрал лишнюю проверку на .PREVIEW

--- src/main/java/com/tgbot/noteskeeperbot/commands/notes/render/NotesPageBuilder.java
+++ src/main/java/com/tgbot/noteskeeperbot/commands/notes/render/NotesPageBuilder.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.List;
 public class NotesPageBuilder {
     private final CallbackButtons callbackButtons;
     private final NoteService noteService;
+    private static final Logger logger = LoggerFactory.getLogger(NotesPageBuilder.class);
 
     public NotesPageBuilder(CallbackButtons callbackButtons, NoteService noteService) {
         this.callbackButtons = callbackButtons;
@@ -25,6 +28,8 @@ public class NotesPageBuilder {
 
     // --------------- Если у юзера нет заметок ----------------------
     public SendMessage getNotesIsEmptyMessage(Long userId) {
+        logger.info("[NotesPageBuilder] Формирую сообщение с уведомлением об отсутствии заметок для пользователя {} ...", userId);
+
         SendMessage message = new SendMessage(userId.toString(), "Ваш список заметок пуст");
 
         InlineKeyboardButton mainMenu = callbackButtons.mainMenuButton();
@@ -35,11 +40,14 @@ public class NotesPageBuilder {
         inlineKeyboardMarkup.setKeyboard(rows);
 
         message.setReplyMarkup(inlineKeyboardMarkup);
+        logger.info("[NotesPageBuilder] Уведомление об отсутствии заметок для пользователя {} готово!", userId);
         return message;
     }
 
     // --------------- Получаем текст и кнопки для пагинации в одном объекте ----------------------
     public NotesPageDTO getFieldsFromDTO(Long userId, int page, String pagePrefix, NotesViewMode notesViewMode) {
+        logger.info("[NotesPageBuilder] Подготавливаю сообщение со списком заметок и пагинацией для пользователя {} ...", userId);
+
         // --------------- Рендерим текст ----------------------
         List<NotesEntity> notes = noteService.getAllUserNotes(userId);
         List<String> pagesList = new ArrayList<>();
@@ -47,39 +55,41 @@ public class NotesPageBuilder {
 
         int i = 1;
         int notesOnPage = 0;
-        String nextNote;
+        String noteBlock;
 
         StringBuilder currentPageSB = new StringBuilder();
 
+        logger.info("[NotesPageBuilder] Начинаю формировать текстовое сообщение для пользователя {} ...", userId);
         if (notesViewMode == NotesViewMode.PREVIEW) {
+
             if (page == 0) {
                 currentPageSB.append("\uD83D\uDCD4 Ваши заметки \uD83D\uDCD4\n\n\n");
             }
 
-            if (notesViewMode == NotesViewMode.PREVIEW) {
-                for (NotesEntity note : notes) {
-                    nextNote = "\uD83D\uDCCB  " + note.getNoteText() + "\n_____________________________________\n\n";
-                    if (currentPageSB.length() + nextNote.length() > 4000 || notesOnPage >= 10) {
-                        pagesList.add(currentPageSB.toString());
-                        currentPageSB = new StringBuilder();
-                        notesOnPage = 0;
-                    }
-                    currentPageSB.append(nextNote);
-                    notesOnPage++;
-                }
-            }
-        } else {
             for (NotesEntity note : notes) {
-                nextNote = "\uD83D\uDCCB  " + i++ + ")  " + note.getNoteText() + "\n_____________________________________\n\n";
-                if (currentPageSB.length() + nextNote.length() > 4000 || notesOnPage >= 10) {
+                noteBlock = "\uD83D\uDCCB  " + note.getNoteText() + "\n_____________________________________\n\n";
+                if (currentPageSB.length() + noteBlock.length() > 4000 || notesOnPage >= 10) {
                     pagesList.add(currentPageSB.toString());
                     currentPageSB = new StringBuilder();
                     notesOnPage = 0;
                 }
-                currentPageSB.append(nextNote);
+                currentPageSB.append(noteBlock);
+                notesOnPage++;
+            }
+
+        } else {
+            for (NotesEntity note : notes) {
+                noteBlock = "\uD83D\uDCCB  " + i++ + ")  " + note.getNoteText() + "\n_____________________________________\n\n";
+                if (currentPageSB.length() + noteBlock.length() > 4000 || notesOnPage >= 10) {
+                    pagesList.add(currentPageSB.toString());
+                    currentPageSB = new StringBuilder();
+                    notesOnPage = 0;
+                }
+                currentPageSB.append(noteBlock);
                 notesOnPage++;
             }
         }
+        logger.info("[NotesPageBuilder] Текстовое сообщение для пользователя {} сформировано!", userId);
 
         if (!currentPageSB.toString().isBlank()) {
             pagesList.add(currentPageSB.toString());
@@ -87,6 +97,8 @@ public class NotesPageBuilder {
 
 
         // --------------- Рендерим кнопки ----------------------
+        logger.info("[NotesPageBuilder] Создаю кнопки пагинации для пользователя {} ...", userId);
+
         List<List<InlineKeyboardButton>> fullKeyboard = new ArrayList<>();
         List<InlineKeyboardButton> paginationRow = new ArrayList<>();
 
@@ -114,7 +126,9 @@ public class NotesPageBuilder {
         if (!paginationRow.isEmpty()) {
             fullKeyboard.add(paginationRow);
         }
+        logger.info("[NotesPageBuilder] Кнопки пагинации для сообщения пользователю {} созданы!", userId);
 
+        logger.info("[NotesPageBuilder] Сообщение со списком заметок и пагинацией для пользователя {} готово!", userId);
         return new NotesPageDTO(pagesList.get(page), fullKeyboard);
     }
 }
