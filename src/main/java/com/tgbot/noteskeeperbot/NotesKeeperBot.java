8bcadd4f08529dfe8d82435bd8265203e9d2f7ba
puh0v <puh7794@gmail.com>
2025-08-12 14:16:04 +0300

    refactor: удалил лишние импорты

--- src/main/java/com/tgbot/noteskeeperbot/NotesKeeperBot.java
+++ src/main/java/com/tgbot/noteskeeperbot/NotesKeeperBot.java
@@ -1,11 +1,7 @@
 package com.tgbot.noteskeeperbot;
 
-
-import com.tgbot.noteskeeperbot.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class NotesKeeperBot {
