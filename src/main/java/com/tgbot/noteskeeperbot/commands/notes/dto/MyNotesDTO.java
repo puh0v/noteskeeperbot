9ade0fa6dc0033bc941c939d7dec391efec821a6
puh0v <puh7794@gmail.com>
2025-08-19 11:06:16 +0300

    refactor(MyNotesDTO): новый класс для улучшения читаемости в MyNotes

---
+++ src/main/java/com/tgbot/noteskeeperbot/commands/notes/dto/MyNotesDTO.java
@@ -0,0 +1,34 @@
+package com.tgbot.noteskeeperbot.commands.notes.dto;
+
+import com.tgbot.noteskeeperbot.services.receiver.TelegramBotService;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyNotesDTO {
+    private final TelegramBotService telegramBotService;
+    private final Update update;
+    private final long userId;
+    private final String userMessage;
+
+    public MyNotesDTO(TelegramBotService telegramBotService, Update update, long userId, String userMessage) {
+        this.telegramBotService = telegramBotService;
+        this.update = update;
+        this.userId = userId;
+        this.userMessage = userMessage;
+    }
+
+    public TelegramBotService getTelegramBotService() {
+        return telegramBotService;
+    }
+
+    public Update getUpdate() {
+        return update;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+}
