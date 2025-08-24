0642bbe00418e8421f1efbbd67e1325ab71902ef
puh0v <puh7794@gmail.com>
2025-08-25 01:10:21 +0300

    refactor(UserRepository): новый метод findAllUserIds();

--- src/main/java/com/tgbot/noteskeeperbot/database/repository/UsersRepository.java
+++ src/main/java/com/tgbot/noteskeeperbot/database/repository/UsersRepository.java
@@ -4,8 +4,12 @@ import com.tgbot.noteskeeperbot.database.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
 
     boolean existsByUserId(Long userId);
+
+    List<Long> findAllUserIds();
 }
